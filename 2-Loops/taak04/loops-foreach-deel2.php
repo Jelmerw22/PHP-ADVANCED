a77d95fe6c9db96aa71adb84dbc1a896f11e183e
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-04-26 12:19:50 +0200

    sheesh

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -1,13 +1,22 @@
+<?php
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+?>
 <!DOCTYPE html>
 <html>
 <body>
-<button>Verzend</button>
-<?php
-$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
-
-foreach ($klassen as $value) {
-    echo  "<ul>$value</ul>";
-}
-?>
+    <label for="Klas">Kies een Klas:</label>
+    <select name="klassen">
+    <?php
+    foreach ($klassen as $value):
+        echo '<option value="'.$klassen.'">'.$value.'</option>';
+    endforeach;
+    ?>
+    </select>
+    <input type="submit">
 </body>
 </html>
+<?php
+foreach ($klassen as $value) {
+    echo "<li>$value</li>";
+}
+?>
