3fa63109d2376abddae5b18ebd21de76080b8f6c
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-04-30 12:38:35 +0200

    php

---
+++ 3-Forms/taak01/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <form name="invoerform" action="invoerform.php" method="POST">
+        Voornaam;
+        <br>
+        <input type="text" name="voornaam">
+        <br>
+        Achternaam;
+        <br>
+        <input type="text" name="achternaam">
+        <br>
+        Klas;
+        <br>
+        <input type="text" name="klas">
+        <br>
+        Leeftijd;
+        <br>
+        <input type="text" name="leeftijd">
+        <br>
+        Adress;
+        <br>
+        <input type="text" name="adress">
+        <br>
+        Plaatsnaam;
+        <br>
+        <input type="text" name="plaatsnaam">
+        <br>
+        Favorieten muziekband;
+        <br>
+        <input type="text" name="favorieten-muziekband">
+        <br>
+        <input type="submit" name="Verstuur antwoord.">
+    </form>
+</body>
+</html>
---
+++ 3-Forms/taak01/invoerform.php
@@ -0,0 +1,15 @@
+<?php
+echo "Voornaam is; ".$_POST["voornaam"];
+echo "<br>";
+echo "Achternaam is; ".$_POST["achternaam"];
+echo "<br>";
+echo "Klas is; ".$_POST["klas"];
+echo "<br>";
+echo "Leeftijd is; ".$_POST["leeftijd"];
+echo "<br>";
+echo "Adress is; ".$_POST["adress"];
+echo "<br>";
+echo "Plaatsnaam is; ".$_POST["plaatsnaam"];
+echo "<br>";
+echo "Favoriete muziekband is; ".$_POST["favorieten-muziekband"]; 
+?>
