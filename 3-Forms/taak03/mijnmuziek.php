a41b1bd352190d774ad7301325cbcd562132349e
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-05-18 12:46:59 +0200

    ez php

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Forms</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+echo "Mijn favorieten artiets is ".$_GET["artiest"];
+echo "<br>";
+echo "De titel van zijn/haar liedje is ".$_GET["titel"];
+echo "<br>";
+echo "Dit liedje kwam uit in ".$_GET["jaar"];
+echo "<br>";
+echo "Het genre is ".$_GET["genre"];
+?>
