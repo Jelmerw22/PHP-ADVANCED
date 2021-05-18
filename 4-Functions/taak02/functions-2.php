fecd864b23a8ecea6182be55ce669bd049053313
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2021-05-18 12:15:48 +0200

    functions2

--- 4-Functions/taak02/functions-2.php
+++ 4-Functions/taak02/functions-2.php
@@ -7,3 +7,41 @@
     
 </body>
 </html>
+
+<?php
+
+
+function Rekennuit(){
+    echo "<br>";
+    $getal1=5;
+    $getal2=54;
+    $getal3=12;
+    $sum = $getal1 + $getal2 + $getal3;
+    echo "Als ik de getallen $getal1, $getal2 en $getal3 bij elkaar op tel krijg ik $sum";
+    echo "<br>";
+}
+
+function Rekennuit2(){
+    echo "<br>";
+    $getal4=100;
+    $getal5=320;
+    $getal6=6969;
+    $sum = $getal4 + $getal5 + $getal6;
+    echo "Als ik de getallen $getal4, $getal5 en $getal6 bij elkaar op tel krijg ik $sum";
+    echo "<br>";
+}
+
+function Rekennuit3(){
+    echo "<br>";
+    $getal7=420;
+    $getal8=360;
+    $getal9=180;
+    $sum = $getal7 + $getal8 + $getal9;
+    echo "Als ik de getallen $getal7, $getal8 en $getal9 bij elkaar op tel krijg ik $sum";
+    echo "<br>";
+}
+Rekennuit();
+Rekennuit2();
+Rekennuit3();
+?>
+
