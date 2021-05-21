579138ceac956c6dae2cdbfa5b77f856beb6e9d5
Jelmer Wiersma <jelmer.wiersma@rocva.nl>
2021-05-21 12:04:02 +0200

    php adv eindopdracht

--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -4,17 +4,71 @@
     <title>PHP Advanced Eindopdracht.</title>
 </head>
 <body>
-    Tekstkleur;
+<form name="invoerform" action="advanced-2.php" method="POST">
+    Achtergrondkleur;
     <br>
-    <select name="lievelingskleur">
+    <select name="achtergrondkleur">
         <?php
             $kleuren = array("red", "blue", "green", "black", "brown");
-
             foreach ($kleuren as $value) {
                 echo  "<option>$value</option>";
-}
+            }
         ?>
     </select>
+    <br>
+
+    Tekstkleur;
+    <br>
+    <select name="tekstkleur">
+        <?php
+            foreach ($kleuren as $value) {
+                echo  "<option>$value</option>";
+            }
+            ?>
+    </select>
+    <br>
+
+    Font variant;
+    <br>
+    <select name="font">
+        <?php
+            $font = array("normal", "small-caps", "all-small-caps", "petite-caps", "all-petite-caps", "unicase");
+            foreach ($font as $value) {
+                echo "<option>$value</option>";
+            }
+        ?>
+    </select>
+    <br>    
+
+    Border style;
+    <br>
+    <select name="borderstyle">
+        <?php
+            $styles= array("none", "groove", "dashed", "solid", "dotted", "double");
+            foreach ($styles as $value) {
+                echo "<option>$value</option>";
+            }
+        ?>
+    </select>
+    <br>
+    Border kleur;
+    <br>
+    <select name="borderkleur">
+        <?php
+            foreach ($kleuren as $value) {
+                echo  "<option>$value</option>";
+            }
+        ?>
+    </select>
+    <br>
+    Table border (px);
+    <input type="text" name="tableborder">
+    <br>
+    Cel-padding (px);
+    <input type="text" name="cel-padding">
+    <br>
+    <input type="submit" name="Verstuur antwoord.">
+</form>
 </body>
 </html>
 
--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -3,7 +3,48 @@
 <head>
     <title>PHP Advanced Eindopdracht.</title>
 </head>
+<style>
+    *{
+        margin: auto;
+        font-variant-caps: <?php echo $_POST["font"]?>;
+    }
+    table, th, td{
+        border: <?php echo $_POST["tableborder"] ?> solid black;
+        padding: <?php echo $_POST["cel-padding"] ?>;
+        border-style: <?php echo $_POST["borderstyle"]?>;
+        border-color: <?php echo $_POST["borderkleur"]?>;
+    }
+
+</style>
 <body>
-    
+    <?php
+        $data = array("Voornaam"=>"Jelmer", "Leeftijd"=>"17", "Klas"=>"0C", "Sport"=>"Voetbal", "Woonplaats"=>"Amstelveen");
+        $achtergrondkleur= $_POST["achtergrondkleur"];
+        echo "<body style ='background-color:$achtergrondkleur'>";
+        $tekstkleur= $_POST["tekstkleur"];
+        echo "<span style='color:$tekstkleur'>";
+    ?>
+
+    <table style="width: 40%;">
+        <tr>
+            <th>Key</th>
+            <th>Value</th>
+        </tr>
+    </table>
+
+    <?php function maakRij($value,$key){ ?>
+        <table style="width: 40%">
+            <tr>
+                <td><?php echo "$value"; ?></td>
+                <td><?php echo "$key"; ?></td>
+            </tr>
+        </table>
+    <?php } ?>
+    <?php
+        foreach ($data as $key => $value) {
+            maakRij("$key", "$value");
+        }
+    ?>
+
 </body>
 </html>
